515374acd5e422761b0475c14e5b9578414d6878
alexwood4real <akwood6@asu.edu>
2026-05-09 14:45:43 -0700

    something super simple

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 // cargo build
 // cargo run
 // ./target/debug/hello_world
+use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess  = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line.");
+
+    println!("You guessed: {}", guess);
 }
